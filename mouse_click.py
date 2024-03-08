2e062ed00bcea26e84635c0b641005fe047b3950
curtis081 <ar06053127hg@gmail.com>
2024-03-08 19:14:03 +0800

    add current_x, current_y saving and type annotations

--- mouse_click.py
+++ mouse_click.py
@@ -1,20 +1,21 @@
 import pyautogui
 import time
 import datetime
+from typing import Tuple
 
 
-def is_time_in_range(start, end, current_time):
+def is_time_in_range(start: datetime.time, end: datetime.time, current_time: datetime.time) -> bool:
     """Check if the current time is within the specified range."""
     return start <= current_time <= end
 
 
-def is_click_time_enabled(start_time, end_time):
+def is_click_time_enabled(start_time: datetime.time, end_time: datetime.time) -> bool:
     """Determine if the current time is within the allowed click time range, based on provided start and end times."""
     now = datetime.datetime.now().time()
     return is_time_in_range(start_time, end_time, now)
 
 
-def get_time_from_user(prompt, default_time=None):
+def get_time_from_user(prompt: str, default_time: datetime.time = None) -> datetime.time:
     """Prompt the user for a time in HH:MM format or use the default time if 'd' is entered."""
     while True:
         user_input = input(prompt).strip()
@@ -27,7 +28,8 @@ def get_time_from_user(prompt, default_time=None):
             print("Invalid format. Please enter the time in HH:MM format or press 'd' for default.")
 
 
-def get_validated_time_range(prompt_start, prompt_end, default_start_time, default_end_time):
+def get_validated_time_range(prompt_start: str, prompt_end: str, default_start_time: datetime.time,
+                             default_end_time: datetime.time) -> Tuple[datetime.time, datetime.time]:
     """Prompt the user for start and end times, ensuring the end time is after the start time, or use default times."""
     while True:
         start_time = get_time_from_user(prompt_start, default_start_time)
@@ -40,7 +42,7 @@ def get_validated_time_range(prompt_start, prompt_end, default_start_time, defau
                   "Please enter the times again or press 'd' for default.")
 
 
-def get_delay_time(prompt, default_delay=180):
+def get_delay_time(prompt: str, default_delay: int = 180) -> int:
     """Prompt the user for a delay time in seconds."""
     while True:
         user_input = input(prompt).strip()
@@ -83,6 +85,8 @@ def mouse_left_click():
         try:
             if is_click_time_enabled(work_start, work_end):
                 pyautogui.click(target_x, target_y)
+                current_x, current_y = pyautogui.position()
+                pyautogui.moveTo(current_x, current_y)
                 print(f'Mouse left clicked at: {target_x}, {target_y}')
             else:
                 print("Outside allowed click time. Skipping click.")
